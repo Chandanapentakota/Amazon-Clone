295cbbeba95e8b0705ac3e9e2ce0e1f6c8de1b50
Chandanapentakota <161708921+Chandanapentakota@users.noreply.github.com>
2024-05-24 14:59:26 +0530

    Add files via upload

---
+++ Amazon/Homepage/Amazon Homepage Images/Amazon.js
@@ -0,0 +1,69 @@
+document.querySelector(".r-btn").addEventListener("click",function(event){
+    document.querySelector(".product-slide").scrollLeft +=1100;
+    event.preventDefault();
+});
+document.querySelector(".l-btn").addEventListener("click",function(event){
+    document.querySelector(".product-slide").scrollLeft -=1100;
+    event.preventDefault();
+});
+document.querySelector(".btn-1a").addEventListener("click",function(event){
+    document.querySelector(".product-slide-1").scrollLeft +=1100;
+    event.preventDefault();
+});
+document.querySelector(".btn-1b").addEventListener("click",function(event){
+    document.querySelector(".product-slide-1").scrollLeft -=1100;
+    event.preventDefault();
+});
+document.querySelector(".btn-2a").addEventListener("click",function(event){
+    document.querySelector(".product-slide-2").scrollLeft +=1100;
+    event.preventDefault();
+});
+document.querySelector(".btn-2b").addEventListener("click",function(event){
+    document.querySelector(".product-slide-2").scrollLeft -=1100;
+    event.preventDefault();
+});
+document.querySelector(".btn-3a").addEventListener("click",function(event){
+    document.querySelector(".product-slide-3").scrollLeft +=1100;
+    event.preventDefault();
+});
+document.querySelector(".btn-3b").addEventListener("click",function(event){
+    document.querySelector(".product-slide-3").scrollLeft -=1100;
+    event.preventDefault();
+});
+
+document.querySelector(".backtop").addEventListener("click",function(event){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
+
+const sidebar = document.querySelector(".sidebar");
+const cross = document.querySelector(".fa-xmark");
+const black = document.querySelector(".black");
+const sidebtn = document.querySelector(".second-1");
+sidebtn.addEventListener("click",()=>{
+    sidebar.classList.add("active");
+    cross.classList.add("active");
+    black.classList.add("active");
+
+    document.body.classList.add("stop-scroll");
+});
+cross.addEventListener("click",()=>{
+    sidebar.classList.remove("active");
+    cross.classList.remove("active");
+    black.classList.remove("active");
+    document.body.classList.remove("stop-scroll");
+});
+
+const sign = document.querySelector(".ac");
+const tri = document.querySelector(".triangle");
+const signin = document.querySelector(".hdn-sign");
+
+sign.addEventListener("click",()=>{
+    black.classList.toggle("active-1");
+    tri.classList.toggle("active");
+    signin.classList.toggle("active");
+
+    document.body.classList.toggle("stop-scroll");
+});
